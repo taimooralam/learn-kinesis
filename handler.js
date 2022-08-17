3a8e7572373daf1fbb7325f7c5d236cd2ee94b5c
Taimoor <taimoor.alam12@gmail.com>
2022-08-17 20:59:59 +0200

    intial commit

---
+++ handler.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports.createOrder = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Create order!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
