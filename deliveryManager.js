8368320bed7a96d84ac2b1b0d499b3255159fd31
Taimoor <taimoor.alam12@gmail.com>
2022-10-01 18:57:36 +0200

    add a queue to test orders

--- deliveryManager.js
+++ deliveryManager.js
@@ -1,12 +1,39 @@
 'use strict'
 
-module.exports.deliveryOrder = ordersFulfilled => {
-    console.log('Delivert order was called');
+const orderManager = require('./orderManager');
 
-    return Promise((resolve) => {
-        setTimeout(() => {
-            console.log('foo');
-            resolve('foo');
-        }, 300);
-    })
+const AWS = require('aws-sdk');
+const sqs = new AWS.SQS({
+    region: process.env.region,
+});
+
+const DELIVERY_COMPANY_QUEUE = process.env.deliveryCompanyQueue;
+
+module.exports.deliveryOrder = ordersFulfilled => {
+    
+    // save to database
+
+    // send to queue
+
+    var ordersFulfilledPromises= [];
+
+    for(let order of ordersFulfilled) {
+        const temp = orderManager.updateOrderForDelivery(order.orderId).then((updatedOrder) => {
+            orderManager.saveOrder(updatedOrder).then(() => {
+                notifyDeliveryCompany(updatedOrder);
+            });
+        });
+
+        ordersFulfilledPromises.push(temp);
+    }
+    return Promise.all(ordersFulfilledPromises);
 };
+
+function notifyDeliveryCompany(order) {
+    const params = {
+        MessageBody: JSON.stringify(order),
+        QueueUrl: DELIVERY_COMPANY_QUEUE
+    };
+
+    return sqs.sendMessage(params).promise();
+}
